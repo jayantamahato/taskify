10f5a69c357aca903d871660459806ef06bd0b02
Jayanta <jayantamahato830@gmail.com>
2024-06-06 12:06:56 +0530

    project screen design

---
+++ lib/view/screens/projects/project_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/appbar.dart';
+import 'widgets/project_card.dart';
+import 'widgets/search.dart';
+
+class ProjectScreen extends StatelessWidget {
+  ProjectScreen({super.key});
+
+  final List data = [
+    {
+      'users': [
+        {'url': 'assets/images/user1.jpg'},
+        {'url': 'assets/images/user2.jpg'},
+        {'url': 'assets/images/user3.jpg'},
+        {'url': 'assets/images/user4.jpg'},
+        {'url': 'assets/images/user5.jpg'},
+      ],
+      'title': 'Kudos Website',
+      'status': 'Pending',
+      'progress': 68
+    },
+    {
+      'users': [
+        {'url': 'assets/images/user5.jpg'},
+        {'url': 'assets/images/user4.jpg'},
+        {'url': 'assets/images/user3.jpg'},
+        {'url': 'assets/images/user2.jpg'},
+      ],
+      'title': 'Araby ai',
+      'status': 'Bused',
+      'progress': 10
+    },
+    {
+      'users': [
+        {'url': 'assets/images/user3.jpg'},
+        {'url': 'assets/images/user4.jpg'},
+        {'url': 'assets/images/user2.jpg'},
+        {'url': 'assets/images/user5.jpg'},
+        {'url': 'assets/images/user2.jpg'},
+        {'url': 'assets/images/user2.jpg'},
+        {'url': 'assets/images/user2.jpg'},
+        {'url': 'assets/images/user2.jpg'},
+      ],
+      'title': 'Kudos Website',
+      'status': 'Pending',
+      'progress': 50
+    }
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: projectAppBar,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CustomSearchBar(),
+            ...data.map(
+              (e) => ProjectCard(
+                  progress: e['progress'],
+                  status: e['status'],
+                  title: e['title'],
+                  users: e['users']),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/screens/projects/widgets/appbar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/theme/font.dart';
+
+AppBar projectAppBar = AppBar(
+  leading: SizedBox(),
+  centerTitle: true,
+  title: Text(
+    'Projects',
+    style: B16,
+  ),
+);
